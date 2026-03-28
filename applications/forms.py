e89ea835c2db23d4f9203e51b972ca84b9994da2
Lokesh <lokeshgosavi1709@gmail.com>
2026-03-28 20:57:20 +0530

    Add SignUpForm to fix CI import error

--- applications/forms.py
+++ applications/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 from .models import JobApplication
 
@@ -17,3 +19,11 @@ class JobForm(forms.ModelForm):
         widgets = {
             'date_applied': forms.DateInput(attrs={'type': 'date'}),
         }
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
